492f33f4f4577df5c6bd50eb32e67d5a26caf0c3
HenryDong123 <943082205@qq.com>
2019-07-17 02:53:54 +0800

    add /api /helpers vuex axios

--- src/pages/Index/template.js
+++ src/pages/Index/template.js
@@ -1,4 +1,6 @@
-
+import request from '@/helpers/request.js'
+import auth from '@/api/auth.js'
+import blog from '@/api/blog.js'
 export default {
   data () {
     return {
--- src/pages/Register/template.vue
+++ src/pages/Register/template.vue
@@ -1,16 +1,16 @@
 <template>
-  <div id="register">
+  <div id="login">
     <h4>用户名</h4>
     <el-input v-model="username" placeholder="用户名"></el-input>
-    <p class="error">当前用户名已注册</p>
+    <!--<p class="error">当前用户名已注册</p>-->
     <h4>密码</h4>
-    <el-input v-model="password" type="password" placeholder="密码" ></el-input>
-    <p class="error">当前用户名已注册</p>
-    <el-button size="medium">立即注册</el-button>
-    <p class="notice">已有账号？<router-link to="/login">立即登录</router-link></p>
+    <el-input v-model="password" type="password" placeholder="密码" @keyup.enter="onRegister" ></el-input>
+    <!--<p class="error">当前用户名已注册</p>-->
+    <el-button size="medium" @click="onRegister">立即注册</el-button>
+    <p class="notice">已有账号？<router-link to="/login">去登录</router-link></p>
   </div>
 </template>
 
 <script src="./template.js"></script>
 
-<style src="../Login/template.less" lang="less"></style>
+<style src="./template.less" lang="less"></style>
