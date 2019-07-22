ea56aa4de20e46751389efc3389da571d34a684b
HenryDong123 <943082205@qq.com>
2019-07-23 01:04:01 +0800

    end

--- src/main.js
+++ src/main.js
@@ -6,7 +6,10 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 import store from './store'
-Vue.use(ElementUI);
+import Util from '@/helpers/util'
+
+Vue.use(ElementUI)
+Vue.use(Util)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/pages/Detail/template.js
+++ src/pages/Detail/template.js
@@ -1,8 +1,27 @@
+import marked from 'marked'
+import blog from '@/api/blog'
+
 export default {
-  data () {
+  data() {
     return {
       title: '',
-      rawContent: ''
+      rawContent: '',
+      user: {},
+      createAt: ''
+    }
+  },
+  created() {
+    blog.getDetail({blogId: this.$route.params.blogId}).then(res => {
+      console.log(res)
+      this.title = res.data.title
+      this.rawContent = res.data.content
+      this.createAt = res.data.user.createdAt
+      this.user = res.data.user
+    })
+  },
+  computed: {
+    markdown() {
+      return marked(this.rawContent)
     }
   }
 }
